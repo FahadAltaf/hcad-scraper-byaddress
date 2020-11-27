9591976d51e863bf9472cc5b97ace22fe1bf9c4f
Fahad Altaf <fahadaltaf10se49@gmail.com>
2020-11-27 19:44:25 +0500

    Initial commit

---
+++ hcad.scraper.byaddress/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hcad.scraper.byaddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
